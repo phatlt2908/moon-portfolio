c5ca388e753fca4c7ad594d0fa9c87303ca7849f
phatlt <phatlt@jienielab.asia>
2022-09-14 12:01:09 +0700

    update navigation bar

--- src/views/components/navigation-bar/navigation-bar.component.jsx
+++ src/views/components/navigation-bar/navigation-bar.component.jsx
@@ -4,18 +4,30 @@ import "./navigation-bar.styles.scss";
 const NavigationBar = () => {
   return (
     <div className="navigation-bar-container">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/portfolio">Portfolio</Link>
-          </li>
-          <li>
-            <Link to="/about">About me</Link>
-          </li>
-        </ul>
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+
+        <div className="navbar-logo">
+          <div className="navbar-item">
+            <Link to="/">O</Link>
+          </div>
+        </div>
+        <div className="navbar-item">
+          <Link to="/portfolio">Portfolio</Link>
+        </div>
+        <div className="navbar-item">
+          <Link to="/about">About me</Link>
+        </div>
+        <div className="navbar-item navbar-right">
+          A
+        </div>
+        <div className="navbar-item">
+          B
+        </div>
+        <div className="navbar-item">
+          <div className="button">
+            Sign up
+          </div>
+        </div>
       </nav>
     </div>
   );
