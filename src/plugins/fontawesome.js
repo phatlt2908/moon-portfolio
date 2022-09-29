34183774e943a44a531bf1225ab7a0c702ef26fc
phatlt <phatlt@jienielab.asia>
2022-09-29 23:45:40 +0700

    update icon in nav and footer

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -4,9 +4,15 @@ import { faMoon } from '@fortawesome/free-solid-svg-icons';
 
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
 import { faPinterestP } from '@fortawesome/free-brands-svg-icons';
+import { faArtstation } from '@fortawesome/free-brands-svg-icons';
+import { faBehance } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 library.add(
     faMoon,
     faFacebookF,
-    faPinterestP
+    faPinterestP,
+    faArtstation,
+    faBehance,
+    faInstagram
 )
